0fe8cb78a9305fc55fa3fdb28c280b75f92a719f
Ashryz <tarek.ashry9894@outlook.com>
2025-11-15 19:42:52 +0200

    [ADD] adding quiz_15_questions

---
+++ quiz1.py
@@ -0,0 +1,132 @@
+############## 1 #####################
+
+# name = input("Enter your name: ")
+# print(f"Hello {name}")
+
+############## 2 #####################
+
+# a,b,c,d = 10, "10", 3.14, True
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+############## 3 #####################
+
+# num1 = int(input("Enter number: "))
+# num2 = int(input("Enter number: "))
+# print(f"Modulas : {num1%num2}")
+
+############## 4 #####################
+
+# c = float(input("Enter temperature in Celsiu: "))
+# print(f"Temp in Fahrenheit: {(c*9/5)+32}")
+
+############## 5 #####################
+
+# x = int(input("Enter number to check even or odd: "))
+# if x%2 == 0 :
+#     print("num is even")
+# else:
+#     print("num is odd")
+
+############## 6 #####################
+
+# x = int(input("Enter first number: "))
+# y = int(input("Enter second number: "))
+# z = int(input("Enter third number: "))
+# if x > y and x > z :
+#     print(f"the largest num :{x}")
+# elif y > x and y > z :
+#     print(f"the largest num :{y}")
+# else:
+#     print(f"the largest num :{z}")
+
+############## 7 #####################
+
+# x = int(input("Enter first number: "))
+# y = int(input("Enter second number: "))
+# op = input("Enter the operation (+ , *, -, /): ")
+# if op == '+':
+#     print(f"result: {x+y}")
+# elif op == '-':
+#     print(f"result: {x-y}")
+# elif op == '*':
+#     print(f"result: {x*y}")
+# elif op == '/':
+#     print(f"result: {x/y}")
+# else:
+#     print("failed, pls enter a valid operation from above!")
+
+############## 8 #####################
+
+# score = int(input("Enter score from (0 - 100): "))
+# if score >= 90 and score <= 100:
+#     print("grade : A")
+# elif score >= 80 and score < 90:
+#     print("grade : B")
+# elif score >= 70 and score < 80:
+#     print("grade : C")
+# elif score >= 60 and score < 70:
+#     print("grade : D")
+# elif score < 60 and score >= 0:
+#     print("grade : F")
+# else:
+#     print("your score is not in the rage!")
+
+############## 9 #####################
+
+# text = 'I love Python'
+# print(text.replace('love','like'))
+
+############## 10 #####################
+
+# nums = [5,2,9,1,7]
+# nums.append(10)
+# nums.pop(1)
+# print(sorted(nums))
+
+############## 11 #####################
+
+# nums = [4,6,1,3,9]
+# sum = 0
+# for num in nums:
+#     sum += num
+# print(f"Sum of this list: {sum}")
+
+############## 12 #####################
+
+# vowels = 'aeiou'
+# text = input("Enter your word to count vowels: ")
+# count = 0
+# for s in text:
+#     if s in vowels:
+#         count += 1
+# print(f"vowels in your text: {count}")
+
+############## 13 #####################
+
+# student = {"name":"Omar","age":20,"grade":"A"}
+# print(student['name'])
+# print(student['age'])
+
+############## 14 #####################
+
+# nums = [12,5,8,20,3]
+# max_num = 0 
+# for num in nums:
+#     if num > max_num:
+#         max_num = num
+# print(f"maximum number in the list: {max_num}")
+
+############## 15 #####################
+
+# fruits = ["apple","banana","mango","orange"]
+# check = input("enter name of fruits to check is exists: ")
+# for f in fruits:
+#     if f == check:
+#         print(f"{f} is in the list")
+#         break
+# else:
+#     print(f"{check} not in the list")
+
